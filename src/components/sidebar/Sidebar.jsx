6baf86adeec06197d3f5b4ab931dabe05f584bac
Ali Fahed <alifahed67@gmail.com>
2022-09-19 20:48:26 +0800

    Add body components

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">sidebar</div>
+    </div>
+  );
+};
+
+export default Sidebar;
